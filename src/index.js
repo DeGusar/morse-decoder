182a030960e4fc46a541e1cac2381e0605e7bcd9
Denis Gusar <degusar.wot@gmail.com>
2021-08-22 21:10:52 +0300

    feat: finish task morse-decoder

--- src/index.js
+++ src/index.js
@@ -34,13 +34,44 @@ const MORSE_TABLE = {
     '--...':  '7',
     '---..':  '8',
     '----.':  '9',
-    '-----':  '0',
+    '-----': '0',
+    ' ': ' ',
 };
 
 function decode(expr) {
-    // write your solution here
+    let exprArray = expr.split('');
+    let substrExpr = [];
+    let morseCode = [];
+    let result = '';
+    let morselet = '';
+    for (let i = 0; i < exprArray.length; i=i+10) {
+        let substrExpr = exprArray.slice(i, i + 10);
+        if (substrExpr[0] === '*') {
+            result = result + ' ';
+        } else {
+            let morseLetter = [];
+            for (let j = 0; j < substrExpr.length - 1; j+=2){
+               
+               if (substrExpr[j] === '1' && substrExpr[j+1] === '0') {
+                    morseLetter.push('.');
+                } else if (substrExpr[j] === '1' && substrExpr[j+1] === '1') {
+                    morseLetter.push('-');
+               } else {
+                   morseLetter.push('');
+                }
+            }
+                for (let key in MORSE_TABLE) {
+                    if (key === morseLetter.join(''))
+                        result = result + MORSE_TABLE[key];
+            }
+        }
+       }
+    return result;
+    
 }
 
 module.exports = {
+    
     decode
+    
 }
